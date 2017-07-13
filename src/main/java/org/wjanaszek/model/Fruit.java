bcfcf00a092a7fa1ed2b5bfe87908d6ac91b3056
wjanaszek <trash.wjanaszek@gmail.com>
2017-07-13 19:14:03 +0200

    Working on...

---
+++ src/main/java/org/wjanaszek/model/Fruit.java
@@ -0,0 +1,67 @@
+package org.wjanaszek.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Fruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    private String fruitId;
+    private String description;
+    private String imageUrl;
+    private BigDecimal price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getFruitId() {
+        return fruitId;
+    }
+
+    public void setFruitId(String fruitId) {
+        this.fruitId = fruitId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
--- src/test/java/org/wjanaszek/FruitszupApplicationTests.java
+++ src/test/java/org/wjanaszek/FruitszupApplicationTests.java
@@ -3,10 +3,13 @@ package org.wjanaszek;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = FruitszupApplication.class)
+@WebAppConfiguration
 public class FruitszupApplicationTests {
 
 	@Test
