5b1a1edbeddcab7339a6f2101069a97e9e2790ce
wjanaszek <trash.wjanaszek@gmail.com>
2017-07-13 18:50:40 +0200

    Initial commit

---
+++ src/main/java/org/wjanaszek/FruitszupApplication.java
@@ -0,0 +1,12 @@
+package org.wjanaszek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitszupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitszupApplication.class, args);
+	}
+}
